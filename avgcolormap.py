7fdbd588bc8c1434962252fca950d766dbc35bf5
mrhaikuswan <ashwinak2011@gmail.com>
2018-12-28 19:58:11 -0500

    Made ColorMap class, quality of life updates

--- avgcolormap.py
+++ avgcolormap.py
@@ -3,46 +3,60 @@ from colorsys import rgb_to_hls
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-
-def unzip(zipped):
-    lists = []
-    for i in range(len(zipped[0])):
-        orig = [x[i] for x in zipped]
-        lists.append(orig)
-    return lists
+from math import sqrt
 
 
-
-#with open('rgbindex.json') as f:
-#    rgbindex = json.load(f)
-
-coordlist = []
-colorlist = []
-
-
-#for key in rgbindex:
-#    rgbtup = tuple([x/255.0 for x in rgbindex[key]])
-#    pttup = rgb_to_hls(rgbtup[0], rgbtup[1], rgbtup[2])[:2]
-##    pttup = (rgb_to_hls(r/255.0, g/255.0, b/255.0)[0], (r+g+b)/765.0)
-#    colorlist.append(rgbtup)
-#    coordlist.append(pttup)
-
-img = Image.open('earth.png')
-img = img.resize((img.size[0]/50,img.size[1]/50), Image.BOX)
-pixels = np.array(img)
-for x in range(len(pixels)):
-    for y in range(len(pixels[x])):
-        rgbtup = tuple([i/255.0 for i in pixels[x,y]])
-        pttup = rgb_to_hls(rgbtup[0], rgbtup[1], rgbtup[2])[:2]
-        colorlist.append(rgbtup)
-        coordlist.append(pttup)
-
-def plot_colormap(coordlist, colorlist, subject_name):
-    coordlist = unzip(coordlist)
-    plt.scatter(coordlist[0], coordlist[1], c = colorlist, s = 5)
-    plt.xlabel('Hue')
-    plt.ylabel('Brightness')
-    plt.savefig('./Colormaps/%s_colormap.png' % (subject_name))
-    plt.show()
-
-plot_colormap(coordlist, colorlist, 'earth')
+class ColorMap(object):
+    
+    def __init__(self):
+        self.coordlist = []
+        self.colorlist = []
+        self.last_loaded_name = ''
+    
+    def plot_colormap(self, subject_name = None, pt_size = 5):
+        if subject_name is None:
+            subject_name = self.last_loaded_name
+        self.coordlist = self.unzip(self.coordlist)
+        plt.scatter(self.coordlist[0], self.coordlist[1], c = self.colorlist, s = pt_size)
+        plt.xlabel('Hue')
+        plt.ylabel('Lightness')
+        plt.savefig('Colormaps/%s_colormap.png' % (subject_name))
+        plt.show()
+    
+    def unzip(self, zipped):
+        lists = []
+        for i in range(len(zipped[0])):
+            orig = [x[i] for x in zipped]
+            lists.append(orig)
+        return lists
+    
+    def load_rgbindex(self, fp = 'rgbindex.json'):
+        self.__init__()
+        self.last_loaded_name = fp.split('.')[0]
+        with open(fp, 'r') as f:
+            rgbindex = json.load(f)
+        for key in rgbindex:
+            rgbtup = tuple([x/255.0 for x in rgbindex[key]])
+            pttup = rgb_to_hls(rgbtup[0], rgbtup[1], rgbtup[2])[:2]
+            self.colorlist.append(rgbtup)
+            self.coordlist.append(pttup)
+    
+    def load_image(self, fp, sq_size = None):
+        self.__init__()
+        self.last_loaded_name = fp.split('.')[0]
+        img = Image.open(fp)
+        if sq_size is None or sq_size <= 1:
+            sq_size = int(sqrt(img.size[0]*img.size[1]) / 45) # calculate sq_size for higher end of ~2000 points of data
+        img = img.resize((img.size[0]/sq_size,img.size[1]/sq_size), Image.BOX)
+        pixels = np.array(img)
+        for x in range(len(pixels)):
+            for y in range(len(pixels[x])):
+                rgbtup = tuple([i/255.0 for i in pixels[x,y]])
+                pttup = rgb_to_hls(rgbtup[0], rgbtup[1], rgbtup[2])[:2]
+                self.colorlist.append(rgbtup)
+                self.coordlist.append(pttup)
+                
+                
+mapper = ColorMap()
+mapper.load_image('earth.png')
+mapper.plot_colormap()
